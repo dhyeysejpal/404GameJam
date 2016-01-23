c747d41ce596d404393a801524517a787c3c4c4b
Sehran Hasanli <sehran.hasanli@mail.utoronto.ca>
2016-01-23 14:16:09 -0500

    added files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,36 @@ public class PlayerMovement : MonoBehaviour
 	public float speed;
 	public float jump_height;
 	public int direction;
+	 GameObject puke;
+	public int h_speed;
+	public int v_speed;
 
 	// Use this for initialization
 	void Start () 
 	{
-		rb = GetComponent<Rigidbody>();
+		rb = GetComponent<Rigidbody>();		
+		puke = GameObject.FindGameObjectWithTag("Puke");
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.Translate(speed * Time.deltaTime * Input.GetAxis("Horizontal"), jump_height * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
-		//GetComponent<SideLock> ().lockZ ();
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		puke.transform.Rotate (horizontal * h_speed, v_speed * vertical, 0);
+
+		if (Input.GetKey (KeyCode.X)) 
+		{
+			transform.Translate(-horizontal * speed * Time.deltaTime, -1.5f*speed * vertical* Time.deltaTime, 0);
+			puke.SetActive (true);
+		} 
+		else 
+		{		
+			transform.Translate(speed * Time.deltaTime * Input.GetAxis("Horizontal"), 0f, 0f);
+			puke.SetActive (false);
+
+		}
 
 	}
 }
